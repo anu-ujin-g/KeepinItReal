603fb5f6cedba77eccd31bf62dae08c65299be2e
Psyrbin <kapustin.andr1@gmail.com>
2020-04-29 13:32:59 -0400

    w2v returns function

--- src/w2v.py
+++ src/w2v.py
@@ -3,24 +3,24 @@ import pandas as pd
 import numpy as np
 
 
-def w2v(df):
+def transform(df):
     w2v = gensim.downloader.load('word2vec-google-news-300')
 
     # Remove punctuation
     df['review'] = df['review'].apply(lambda x: ''.join([c for c in x if c.isalpha() or c==' ']))
 
     # Remove words that have no vector representations
-    df['review'] = df['review'].apply(lambda x: ' '.join(word for word in x.split() if word in model.vocab))
+    df['review'] = df['review'].apply(lambda x: ' '.join(word for word in x.split() if word in w2v.vocab))
 
 
-    w2v = []
+    res = []
     for idx, row in df.iterrows():
         if row.review == '':
-            w2v.append(np.zeros(300))
+            res.append(np.zeros(300))
         else:
-            w2v.append(np.mean([w2v.word_vec(word) for word in row.review.split()]))
-    w2v = np.array(w2v)
+            res.append(np.mean([w2v.word_vec(word) for word in row.review.split()], axis=0))
+    res = np.array(res)
 
 
 
-    return w2v
+    return w2v, res
